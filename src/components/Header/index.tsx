7c8e5054f372e65c3f0fbe030a1c76da01fde61b
lucasptcastro <lucasptcastro@gmail.com>
2023-05-08 15:25:04 -0300

    ✨ feat: created header component

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Index: React.FC = () => {
+  return (
+    <div className="fixed flex justify-between px-72 py-1 text-sm bg-[#27ab83] text-white w-full z-50">
+      <p>(84)99999-9999</p>
+      <p>Só hoje, frete grátis para todo o Brasil</p>
+      <p>Redes sociais</p>
+    </div>
+  );
+};
+
+export default Index;
