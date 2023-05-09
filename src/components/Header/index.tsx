69e760b23a5432386fc5cc3bda71c34f21365cde
lucasptcastro <lucasptcastro@gmail.com>
2023-05-09 15:36:35 -0300

    ✨ feat: added mobile responsiviness

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const Index: React.FC = () => {
   return (
-    <div className="fixed flex justify-between px-72 py-1 text-sm bg-[#27ab83] text-white w-full z-50">
-      <p>(84)99999-9999</p>
+    <div className="fixed flex justify-between px-[15.5%] py-[0.3%] text-sm bg-[#27ab83] text-white w-full z-50 phones:py-0 phones:px-7">
+      <p className="phones:hidden">(84)99999-9999</p>
       <p>Só hoje, frete grátis para todo o Brasil</p>
       <p>Redes sociais</p>
     </div>
