4b2a16f80f2d58ca24abfe768cf42b2d9991ed28
lucasptcastro <lucasptcastro@gmail.com>
2023-05-09 15:34:59 -0300

    ✨ feat: added keyframes and animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      keyframes: {
+        showRight: {
+          "100%": { width: 0 },
+        },
       },
+      animation: {
+        showRight: "showRight 1s ease forwards;",
+      },
+    },
+    screens: {
+      phones: { min: "320px", max: "480px" },
     },
   },
   plugins: [],
-}
+};
