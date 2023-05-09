108193280efe1830f58508b446d7504034f00cfe
lucasptcastro <lucasptcastro@gmail.com>
2023-05-09 15:35:20 -0300

    ✨ feat: adjusted responsiviness

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,8 @@ import Header from "../components/Header";
 import { BsCart2 } from "react-icons/bs";
 // AntDesign
 import { Button, Row, Col, Card } from "antd";
-
+// CSS
+import styles from "./page.module.css";
 const { Meta } = Card;
 
 export default function Home() {
@@ -70,41 +71,45 @@ export default function Home() {
       </div>
 
       {/* Main */}
-      <div className="flex flex-col px-48 bg-[#EFFCF6] h-max">
+      <div className="flex flex-col px-[10%] bg-[#EFFCF6] h-max phones:justify-center">
         {/* Logo + Carrinho */}
-        <div className="flex flex-row justify-between items-center py-20 px-24">
-          <a href="#" className="text-3xl text-black font-bold">
+        <div className="flex flex-row justify-between items-center py-20 px-[7%] phones:px-0">
+          <a href="#" className="text-3xl text-black font-bold phones:text-xl">
             E-commerce
           </a>
           <div className="flex flex-row items-center gap-2">
-            <BsCart2 className="text-3xl" />
-            <p>Carrinho</p>
+            <BsCart2 className="text-3xl phones:text-xl" />
+            <p className="phones:text-sm">Carrinho</p>
           </div>
         </div>
 
         {/* Card principal */}
-        <div className="flex flex-col gap-10 py-10 px-10 mx-24 rounded-xl bg-[#8EEDC7]">
-          <h3 className="text-2xl font-bold">
+        <div className="flex flex-col gap-10 py-10 px-[3%] mx-[7%] rounded-xl bg-[#8EEDC7] phones:mx-0">
+          <h3 className="text-2xl font-bold phones:text-center phones:text-base">
             Olá, somos o E-commerce! Somos 100% nacional e o maior revendedor de
             produtos eletrônicos do país!
           </h3>
-          <p className="text-xl">
+          <p className="text-xl phones:text-center phones:text-base">
             Contate nossos revendedores pelo núnero: <br />
             (84) 99999-9999
           </p>
-          <button className="w-40 h-10 rounded-xl bg-blue-600 text-white hover:bg-blue-500">
+          <button className="w-40 h-10 rounded-xl bg-blue-600 text-white hover:bg-blue-500 phones:mx-auto">
             Comprar agora
           </button>
         </div>
-
         {/* Produtos */}
-        <div className="py-10 px-24 flex flex-col gap-10">
-          <h1 className="font-bold text-2xl">Confira nossos produtos</h1>
-          <Row gutter={[16, 24]}>
+        <div className="flex flex-col py-10 px-[7%] gap-10 phones:w-full phones:px-0">
+          <h1 className="font-bold text-2xl phones:text-center">
+            Confira nossos produtos
+          </h1>
+          <Row
+            gutter={[16, 24]}
+            className="justify-center items-center p-0 phones:flex-col"
+          >
             {produtos.map((value, key) => (
-              <Col key={key} span={6}>
+              <Col key={key} span={6} className="flex flex-row justify-center">
                 <Card
-                  className="w-80"
+                  className="w-full shadow-md phones:min-w-max"
                   hoverable
                   cover={
                     <Image
@@ -124,11 +129,15 @@ export default function Home() {
                     }
                     description="Lorem ipsum dollor at amet ipsum dollor lorem"
                   />
-                  <div className="flex justify-between">
-                    <div></div>
-                    <div></div>
+                  <div className="flex justify-end gap-[2%] items-center py-[1%]">
+                    <div className={styles.addedToCar}>
+                      <p>
+                        <span></span>
+                        Adicionar ao carrinho
+                      </p>
+                    </div>
                     <div>
-                      <BsCart2 className="text-lg hover:text-blue-700" />
+                      <BsCart2 className="text-lg hover:text-blue-700 hover:-rotate-12" />
                     </div>
                   </div>
                 </Card>
