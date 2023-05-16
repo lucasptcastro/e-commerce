4ce9ae5d9e94920edbef6ca8d23d364ecf3dcc1c
lucasptcastro <lucasptcastro@gmail.com>
2023-05-16 10:37:51 -0300

    ✨ feat: added CarProvider

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,10 @@
 import "../styles/global.css";
+import { CarProvider } from "../context/CarContext";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <CarProvider>
+      <Component {...pageProps} />
+    </CarProvider>
+  );
 }
