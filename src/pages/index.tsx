c2d4e5b3608eab79bb109a3bbc8f7762ab100515
lucasptcastro <lucasptcastro@gmail.com>
2023-05-16 15:43:02 -0300

    ✨ feat: added addProductToCar function

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,110 +2,118 @@
 // Next
 import Image from "next/image";
 import Link from "next/link";
-import React, { useContext } from "react";
-
+import React from "react";
 // Components
 import Layout from "./layout";
-import { useCarContext, CarProvider } from "../context/CarContext";
-
+import { useCarContext } from "../context/CarContext";
 // Icons
 import { BsCart2 } from "react-icons/bs";
-import { BsFillCartFill } from "react-icons/bs";
-
-// CSS
-import styles from "./index.module.css";
-
 // AntDesign
-import { Button, Row, Col, Card } from "antd";
+import { Card } from "antd";
 const { Meta } = Card;
 
-export default function Home() {
-  const [carProducts, setCarProducts] = React.useState(0);
-  const produtos = [
-    {
-      produto: "produto1",
-      valor: "392,00",
-    },
-    {
-      produto: "produto2",
-      valor: "41,00",
-    },
-    {
-      produto: "produto3",
-      valor: "63,00",
-    },
-    {
-      produto: "produto4",
-      valor: "21,00",
-    },
-    {
-      produto: "produto5",
-      valor: "62,00",
-    },
-    {
-      produto: "produto6",
-      valor: "123,00",
-    },
-    {
-      produto: "produto7",
-      valor: "65,00",
-    },
-    {
-      produto: "produto8",
-      valor: "83,00",
-    },
-    {
-      produto: "produto9",
-      valor: "49,00",
-    },
-    {
-      produto: "produto10",
-      valor: "423,00",
-    },
-    {
-      produto: "produto11",
-      valor: "18,00",
-    },
-    {
-      produto: "produto12",
-      valor: "194,00",
-    },
-  ];
+interface IProductSelected {
+  id?: number;
+  name?: string;
+  value?: string;
+  quantity?: number;
+}
 
+export default function Home() {
+  // useContext
   const carContext = useCarContext();
-  const carContextTotal: any = carContext[0];
   const carContextToggleTotal: any = carContext[1];
 
-  console.log(carContext);
+  // Products
+  const produtos = [
+    {
+      id: 1,
+      name: "produto1",
+      price: "392,00",
+    },
+    {
+      id: 2,
+      name: "produto2",
+      price: "41,00",
+    },
+    {
+      id: 3,
+      name: "produto3",
+      price: "63,00",
+    },
+    {
+      id: 4,
+      name: "produto4",
+      price: "21,00",
+    },
+    {
+      id: 5,
+      name: "produto5",
+      price: "62,00",
+    },
+    {
+      id: 6,
+      name: "produto6",
+      price: "123,00",
+    },
+    {
+      id: 7,
+      name: "produto7",
+      price: "65,00",
+    },
+    {
+      id: 8,
+      name: "produto8",
+      price: "83,00",
+    },
+    {
+      id: 9,
+      name: "produto9",
+      price: "49,00",
+    },
+    {
+      id: 10,
+      name: "produto10",
+      price: "423,00",
+    },
+    {
+      id: 11,
+      name: "produto11",
+      price: "18,00",
+    },
+    {
+      id: 12,
+      name: "produto12",
+      price: "194,00",
+    },
+  ];
+  const productsSelecteds: Array<IProductSelected> = [{}];
+  function addProductToCar(
+    id: number,
+    name: string,
+    value: string,
+    quantity: number
+  ) {
+    let index = productsSelecteds.findIndex((value) => value.id == id);
+    if (index < 0) {
+      productsSelecteds.push({
+        id: id,
+        name: name,
+        value: value,
+        quantity: quantity,
+      });
+      carContextToggleTotal();
+      console.log(productsSelecteds);
+    }
+  }
 
   return (
-    <CarProvider>
-      <Layout>
+    <>
+      <Layout props={"Home"}>
         <div className="flex flex-col gap-12">
-          {/* Logo + Carrinho */}
-          <div className="flex flex-row justify-between items-center">
-            <Link
-              href="/"
-              className="text-3xl text-black font-bold phones:text-xl"
-            >
-              E-commerce
-            </Link>
-            <div className="flex flex-row items-center gap-5">
-              <div className="flex justify-center items-center text-center">
-                <BsFillCartFill
-                  className="absolute text-4xl phones:text-3xl"
-                  color="#014D40"
-                />
-                <span className="absolute text-[1.8vh] text-white phones:text-sm">
-                  {carContextTotal}
-                </span>
-              </div>
-              <p className="phones:text-sm">Carrinho</p>
-            </div>
-          </div>
-
-          {/* Card principal */}
+          {/* Principal card */}
           <div className="flex flex-col gap-10 py-10 px-[3%] rounded-xl bg-[#8EEDC7]">
+            {/* Informations banner */}
             <h3 className="text-2xl font-bold phones:text-center phones:text-base">
               Olá, somos o E-commerce! Somos 100% nacional e o maior revendedor
               de produtos eletrônicos do país!
@@ -114,62 +122,80 @@ export default function Home() {
               Contate nossos revendedores pelo núnero: <br />
               (84) 99999-9999
             </p>
-            <button className="w-40 h-10 rounded-xl bg-blue-600 text-white hover:bg-blue-500 phones:mx-auto">
-              Comprar agora
-            </button>
+
+            {/* Buy now button */}
+            <a href="#products">
+              <button className="w-40 h-10 rounded-xl bg-blue-600 text-white hover:bg-blue-500 phones:mx-auto">
+                Comprar agora
+              </button>
+            </a>
           </div>
 
-          {/* Produtos */}
-          <div className="flex flex-col gap-10 phones:w-full">
+          {/* Products */}
+          <section className="flex flex-col gap-10 phones:w-full" id="products">
             <h1 className="font-bold text-2xl phones:text-center">
               Confira nossos produtos
             </h1>
 
             <div className="flex flex-wrap gap-3 items-center w-full responsive:justify-center">
               {produtos.map((product, key) => (
-                <Link href={`/produto/${key + 1}`} key={key}>
-                  <Card
-                    className="w-[315px] shadow-md rounded-2xl"
-                    hoverable
-                    cover={
+                <Card
+                  key={key}
+                  className="w-[315px] shadow-md rounded-2xl"
+                  hoverable
+                  cover={
+                    <Link href={`/produto/${product.id}`}>
                       <Image
                         className="w-full h-[200px] object-cover object-center responsive:h-fit"
                         alt="example"
-                        src={`/images/card${key + 1}.jpg`}
+                        src={`/images/card${product.id}.jpg`}
                         width={500}
                         height={500}
                       />
+                    </Link>
+                  }
+                >
+                  <Meta
+                    title={
+                      <div className="flex justify-between">
+                        <p className="font-bold">{product.name}</p>
+                        <p>R${product.price}</p>
+                      </div>
                     }
-                  >
-                    <Meta
-                      title={
-                        <div className="flex justify-between">
-                          <p className="font-bold">{product.produto}</p>
-                          <p>R${product.valor}</p>
-                        </div>
-                      }
-                      description="Lorem ipsum dollor at amet ipsum dollor lorem"
-                    />
-                    <div className="flex justify-between items-center pt-[5%]">
+                    description="Lorem ipsum dollor at amet ipsum dollor lorem"
+                  />
+                  <div className="flex flex-row justify-between items-center pt-[5%] text-xs">
+                    {/* Item in car */}
+                    <div>
                       <p className="text-white bg-[#27AB83] rounded-xl px-2">
                         Item no carrinho
                       </p>
-                      {/* <p>
-                          <span></span>
-                          Adicionar ao carrinho
-                        </p> */}
-                      <BsCart2
-                        className="text-lg hover:text-blue-700 hover:-rotate-12 absolute right-5"
-                        onClick={carContextToggleTotal}
-                      />
                     </div>
-                  </Card>
-                </Link>
+                    {/* Add to car */}
+                    <div
+                      className="flex items-center gap-[1vh] hover:text-blue-700"
+                      onClick={() => {
+                        addProductToCar(
+                          product.id,
+                          product.name,
+                          product.price,
+                          1
+                        );
+                      }}
+                    >
+                      <p>
+                        <span></span>
+                        Adicionar ao carrinho
+                      </p>
+                      <BsCart2 />
+                    </div>
+                  </div>
+                </Card>
               ))}
             </div>
-          </div>
+          </section>
         </div>
       </Layout>
-    </CarProvider>
+    </>
   );
 }
