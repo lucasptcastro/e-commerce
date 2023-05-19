19006a3c22e035a266bfdde53f6914f59f6d2e33
lucasptcastro <lucasptcastro@gmail.com>
2023-05-19 11:29:06 -0300

    🦄 refactor: renamed

---
+++ src/lib/products.tsx
@@ -0,0 +1,646 @@
+const products = [
+  {
+    id: 1,
+    name: "Notebook",
+    price: 392.0,
+    description: "Produto de alta qualidade",
+    informations: [
+      {
+        id: "01",
+        information: "Produto da marca LP Inovações Tecnológicas",
+      },
+      {
+        id: "02",
+        information: "Produto da marca LP Inovações Tecnológicas",
+      },
+      {
+        id: "03",
+        information: "Produto da marca LP Inovações Tecnológicas",
+      },
+      {
+        id: "04",
+        information: "Produto da marca LP Inovações Tecnológicas",
+      },
+    ],
+    details: [
+      {
+        id: "01",
+        detail: "Produto da marca LP Inovações Tecnológicas",
+      },
+      {
+        id: "02",
+        detail: "Produto da marca LP Inovações Tecnológicas",
+      },
+      {
+        id: "03",
+        detail: "Produto da marca LP Inovações Tecnológicas",
+      },
+      {
+        id: "04",
+        detail: "Produto da marca LP Inovações Tecnológicas",
+      },
+      {
+        id: "05",
+        detail: "Produto da marca LP Inovações Tecnológicas",
+      },
+    ],
+  },
+  {
+    id: 2,
+    name: "Camera",
+    price: 41.55,
+    description: "Produto de alta qualidade",
+    informations: [
+      {
+        id: "01",
+        information: "Produto da marca LP Inovações Tecnológicas",
+      },
+      {
+        id: "02",
+        information: "Produto da marca LP Inovações Tecnológicas",
+      },
+      {
+        id: "03",
+        information: "Produto da marca LP Inovações Tecnológicas",
+      },
+      {
+        id: "04",
+        information: "Produto da marca LP Inovações Tecnológicas",
+      },
+    ],
+    details: [
+      {
+        id: "01",
+        detail: "Produto da marca LP Inovações Tecnológicas",
+      },
+      {
+        id: "02",
+        detail: "Produto da marca LP Inovações Tecnológicas",
+      },
+      {
+        id: "03",
+        detail: "Produto da marca LP Inovações Tecnológicas",
+      },
+      {
+        id: "04",
+        detail: "Produto da marca LP Inovações Tecnológicas",
+      },
+      {
+        id: "05",
+        detail: "Produto da marca LP Inovações Tecnológicas",
+      },
+    ],
+  },
+  {
+    id: 3,
+    name: "Card machine",
+    price: 63.0,
+    description: "Produto de alta qualidade",
+    informations: [
+      {
+        id: "01",
+        information: "Produto da marca LP Inovações Tecnológicas",
+      },
+      {
+        id: "02",
+        information: "Produto da marca LP Inovações Tecnológicas",
+      },
+      {
+        id: "03",
+        information: "Produto da marca LP Inovações Tecnológicas",
+      },
+      {
+        id: "04",
+        information: "Produto da marca LP Inovações Tecnológicas",
+      },
+    ],
+    details: [
+      {
+        id: "01",
+        detail: "Produto da marca LP Inovações Tecnológicas",
+      },
+      {
+        id: "02",
+        detail: "Produto da marca LP Inovações Tecnológicas",
+      },
+      {
+        id: "03",
+        detail: "Produto da marca LP Inovações Tecnológicas",
+      },
+      {
+        id: "04",
+        detail: "Produto da marca LP Inovações Tecnológicas",
+      },
+      {
+        id: "05",
+        detail: "Produto da marca LP Inovações Tecnológicas",
+      },
+    ],
+  },
+  {
+    id: 4,
+    name: "Wallet",
+    price: 21.0,
+    description: "Produto de alta qualidade",
+    informations: [
+      {
+        id: "01",
+        information: "Produto da marca LP Inovações Tecnológicas",
+      },
+      {
+        id: "02",
+        information: "Produto da marca LP Inovações Tecnológicas",
+      },
+      {
+        id: "03",
+        information: "Produto da marca LP Inovações Tecnológicas",
+      },
+      {
+        id: "04",
+        information: "Produto da marca LP Inovações Tecnológicas",
+      },
+    ],
+    details: [
+      {
+        id: "01",
+        detail: "Produto da marca LP Inovações Tecnológicas",
+      },
+      {
+        id: "02",
+        detail: "Produto da marca LP Inovações Tecnológicas",
+      },
+      {
+        id: "03",
+        detail: "Produto da marca LP Inovações Tecnológicas",
+      },
+      {
+        id: "04",
+        detail: "Produto da marca LP Inovações Tecnológicas",
+      },
+      {
+        id: "05",
+        detail: "Produto da marca LP Inovações Tecnológicas",
+      },
+    ],
+  },
+  {
+    id: 5,
+    name: "Kit upgrade",
+    price: 62.0,
+    description: "Produto de alta qualidade",
+    informations: [
+      {
+        id: "01",
+        information: "Produto da marca LP Inovações Tecnológicas",
+      },
+      {
+        id: "02",
+        information: "Produto da marca LP Inovações Tecnológicas",
+      },
+      {
+        id: "03",
+        information: "Produto da marca LP Inovações Tecnológicas",
+      },
+      {
+        id: "04",
+        information: "Produto da marca LP Inovações Tecnológicas",
+      },
+    ],
+    details: [
+      {
+        id: "01",
+        detail: "Produto da marca LP Inovações Tecnológicas",
+      },
+      {
+        id: "02",
+        detail: "Produto da marca LP Inovações Tecnológicas",
+      },
+      {
+        id: "03",
+        detail: "Produto da marca LP Inovações Tecnológicas",
+      },
+      {
+        id: "04",
+        detail: "Produto da marca LP Inovações Tecnológicas",
+      },
+      {
+        id: "05",
+        detail: "Produto da marca LP Inovações Tecnológicas",
+      },
+    ],
+  },
+  {
+    id: 6,
+    name: "Smartphone",
+    price: 123.0,
+    description: "Produto de alta qualidade",
+    informations: [
+      {
+        id: "01",
+        information: "Produto da marca LP Inovações Tecnológicas",
+      },
+      {
+        id: "02",
+        information: "Produto da marca LP Inovações Tecnológicas",
+      },
+      {
+        id: "03",
+        information: "Produto da marca LP Inovações Tecnológicas",
+      },
+      {
+        id: "04",
+        information: "Produto da marca LP Inovações Tecnológicas",
+      },
+    ],
+    details: [
+      {
+        id: "01",
+        detail: "Produto da marca LP Inovações Tecnológicas",
+      },
+      {
+        id: "02",
+        detail: "Produto da marca LP Inovações Tecnológicas",
+      },
+      {
+        id: "03",
+        detail: "Produto da marca LP Inovações Tecnológicas",
+      },
+      {
+        id: "04",
+        detail: "Produto da marca LP Inovações Tecnológicas",
+      },
+      {
+        id: "05",
+        detail: "Produto da marca LP Inovações Tecnológicas",
+      },
+    ],
+  },
+  {
+    id: 7,
+    name: "Virtual reality glasses",
+    price: 65.0,
+    description: "Produto de alta qualidade",
+    informations: [
+      {
+        id: "01",
+        information: "Produto da marca LP Inovações Tecnológicas",
+      },
+      {
+        id: "02",
+        information: "Produto da marca LP Inovações Tecnológicas",
+      },
+      {
+        id: "03",
+        information: "Produto da marca LP Inovações Tecnológicas",
+      },
+      {
+        id: "04",
+        information: "Produto da marca LP Inovações Tecnológicas",
+      },
+    ],
+    details: [
+      {
+        id: "01",
+        detail: "Produto da marca LP Inovações Tecnológicas",
+      },
+      {
+        id: "02",
+        detail: "Produto da marca LP Inovações Tecnológicas",
+      },
+      {
+        id: "03",
+        detail: "Produto da marca LP Inovações Tecnológicas",
+      },
+      {
+        id: "04",
+        detail: "Produto da marca LP Inovações Tecnológicas",
+      },
+      {
+        id: "05",
+        detail: "Produto da marca LP Inovações Tecnológicas",
+      },
+    ],
+  },
+  {
+    id: 8,
+    name: "Microphone",
+    price: 83.0,
+    description: "Produto de alta qualidade",
+    informations: [
+      {
+        id: "01",
+        information: "Produto da marca LP Inovações Tecnológicas",
+      },
+      {
+        id: "02",
+        information: "Produto da marca LP Inovações Tecnológicas",
+      },
+      {
+        id: "03",
+        information: "Produto da marca LP Inovações Tecnológicas",
+      },
+      {
+        id: "04",
+        information: "Produto da marca LP Inovações Tecnológicas",
+      },
+    ],
+    details: [
+      {
+        id: "01",
+        detail: "Produto da marca LP Inovações Tecnológicas",
+      },
+      {
+        id: "02",
+        detail: "Produto da marca LP Inovações Tecnológicas",
+      },
+      {
+        id: "03",
+        detail: "Produto da marca LP Inovações Tecnológicas",
+      },
+      {
+        id: "04",
+        detail: "Produto da marca LP Inovações Tecnológicas",
+      },
+      {
+        id: "05",
+        detail: "Produto da marca LP Inovações Tecnológicas",
+      },
+    ],
+  },
+  {
+    id: 9,
+    name: "Phone",
+    price: 49.0,
+    description: "Produto de alta qualidade",
+    informations: [
+      {
+        id: "01",
+        information: "Produto da marca LP Inovações Tecnológicas",
+      },
+      {
+        id: "02",
+        information: "Produto da marca LP Inovações Tecnológicas",
+      },
+      {
+        id: "03",
+        information: "Produto da marca LP Inovações Tecnológicas",
+      },
+      {
+        id: "04",
+        information: "Produto da marca LP Inovações Tecnológicas",
+      },
+    ],
+    details: [
+      {
+        id: "01",
+        detail: "Produto da marca LP Inovações Tecnológicas",
+      },
+      {
+        id: "02",
+        detail: "Produto da marca LP Inovações Tecnológicas",
+      },
+      {
+        id: "03",
+        detail: "Produto da marca LP Inovações Tecnológicas",
+      },
+      {
+        id: "04",
+        detail: "Produto da marca LP Inovações Tecnológicas",
+      },
+      {
+        id: "05",
+        detail: "Produto da marca LP Inovações Tecnológicas",
+      },
+    ],
+  },
+  {
+    id: 10,
+    name: "Headset",
+    price: 423.0,
+    description: "Produto de alta qualidade",
+    informations: [
+      {
+        id: "01",
+        information: "Produto da marca LP Inovações Tecnológicas",
+      },
+      {
+        id: "02",
+        information: "Produto da marca LP Inovações Tecnológicas",
+      },
+      {
+        id: "03",
+        information: "Produto da marca LP Inovações Tecnológicas",
+      },
+      {
+        id: "04",
+        information: "Produto da marca LP Inovações Tecnológicas",
+      },
+    ],
+    details: [
+      {
+        id: "01",
+        detail: "Produto da marca LP Inovações Tecnológicas",
+      },
+      {
+        id: "02",
+        detail: "Produto da marca LP Inovações Tecnológicas",
+      },
+      {
+        id: "03",
+        detail: "Produto da marca LP Inovações Tecnológicas",
+      },
+      {
+        id: "04",
+        detail: "Produto da marca LP Inovações Tecnológicas",
+      },
+      {
+        id: "05",
+        detail: "Produto da marca LP Inovações Tecnológicas",
+      },
+    ],
+  },
+  {
+    id: 11,
+    name: "Mouse",
+    price: 18.0,
+    description: "Produto de alta qualidade",
+    informations: [
+      {
+        id: "01",
+        information: "Produto da marca LP Inovações Tecnológicas",
+      },
+      {
+        id: "02",
+        information: "Produto da marca LP Inovações Tecnológicas",
+      },
+      {
+        id: "03",
+        information: "Produto da marca LP Inovações Tecnológicas",
+      },
+      {
+        id: "04",
+        information: "Produto da marca LP Inovações Tecnológicas",
+      },
+    ],
+    details: [
+      {
+        id: "01",
+        detail: "Produto da marca LP Inovações Tecnológicas",
+      },
+      {
+        id: "02",
+        detail: "Produto da marca LP Inovações Tecnológicas",
+      },
+      {
+        id: "03",
+        detail: "Produto da marca LP Inovações Tecnológicas",
+      },
+      {
+        id: "04",
+        detail: "Produto da marca LP Inovações Tecnológicas",
+      },
+      {
+        id: "05",
+        detail: "Produto da marca LP Inovações Tecnológicas",
+      },
+    ],
+  },
+  {
+    id: 12,
+    name: "Monitor",
+    price: 194.0,
+    description: "Produto de alta qualidade",
+    informations: [
+      {
+        id: "01",
+        information: "Produto da marca LP Inovações Tecnológicas",
+      },
+      {
+        id: "02",
+        information: "Produto da marca LP Inovações Tecnológicas",
+      },
+      {
+        id: "03",
+        information: "Produto da marca LP Inovações Tecnológicas",
+      },
+      {
+        id: "04",
+        information: "Produto da marca LP Inovações Tecnológicas",
+      },
+    ],
+    details: [
+      {
+        id: "01",
+        detail: "Produto da marca LP Inovações Tecnológicas",
+      },
+      {
+        id: "02",
+        detail: "Produto da marca LP Inovações Tecnológicas",
+      },
+      {
+        id: "03",
+        detail: "Produto da marca LP Inovações Tecnológicas",
+      },
+      {
+        id: "04",
+        detail: "Produto da marca LP Inovações Tecnológicas",
+      },
+      {
+        id: "05",
+        detail: "Produto da marca LP Inovações Tecnológicas",
+      },
+    ],
+  },
+];
+
+export function getAllPostIds() {
+  return [
+    {
+      params: {
+        id: "1",
+      },
+    },
+    {
+      params: {
+        id: "2",
+      },
+    },
+    {
+      params: {
+        id: "3",
+      },
+    },
+    {
+      params: {
+        id: "4",
+      },
+    },
+    {
+      params: {
+        id: "5",
+      },
+    },
+    {
+      params: {
+        id: "6",
+      },
+    },
+    {
+      params: {
+        id: "7",
+      },
+    },
+    {
+      params: {
+        id: "8",
+      },
+    },
+    {
+      params: {
+        id: "9",
+      },
+    },
+    {
+      params: {
+        id: "10",
+      },
+    },
+    {
+      params: {
+        id: "11",
+      },
+    },
+    {
+      params: {
+        id: "12",
+      },
+    },
+  ];
+}
+
+export function getProductData(id: number) {
+  const data = products.filter((product) => product.id == id);
+
+  // Return id and data of the products
+  return {
+    id,
+    data,
+  };
+}
+
+export function getAllProducts() {
+  // Return a array of products
+  return products;
+}
+
+export function getProductInformationsById(id: number) {
+  const productArray = products.filter((product) => product.id === id);
+
+  return productArray[0].informations;
+}
+
+export function getProductDetailsById(id: number) {
+  const productArray = products.filter((product) => product.id === id);
+
+  return productArray[0].details;
+}
